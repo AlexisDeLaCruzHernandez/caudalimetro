4d89c33e68e1ebae339fb1d2a817bf29253f2df5
AlexisDeLaCruzHernandez <alexisdelacruzhernandez@gmail.com>
2026-08-29 19:45:01 -0300

    feat: añadir funcion de borrado y litros maximos por intervalo

--- esp32_caudalimetro/main/init.c
+++ esp32_caudalimetro/main/init.c
@@ -2,3 +2,19 @@
 #include "esp_log.h"
 
 static const char *TAG = "INIT";
+
+void borrar_todos_los_archivos(const char *path) {
+    DIR *dir = opendir(path);
+    if (dir == NULL) return;
+
+    struct dirent *entry;
+    char filepath[300];
+
+    ESP_LOGI(TAG, "--- Eliminando todos los archivos ---");
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) continue;
+        snprintf(filepath, sizeof(filepath), "%s/%s", path, entry->d_name);
+        remove(filepath);
+    }
+    closedir(dir);
+}
--- esp32_caudalimetro/main/init.h
+++ esp32_caudalimetro/main/init.h
@@ -2,9 +2,13 @@
 #define INIT_H
 
 #include <stdio.h>
+#include <dirent.h>
 
-#define BUFFER_SIZE     256
-#define INTERVAL_MIN    15
-#define INTERVAL_SEC    (INTERVAL_MIN * 60) 
+#define BUFFER_SIZE         256
+#define INTERVAL_MIN        15
+#define INTERVAL_SEC        (INTERVAL_MIN * 60) 
+#define INTERVAL_MAX_LITER  ((5000 / 60) * INTERVAL_MIN)
+
+void borrar_todos_los_archivos(const char *path);
 
 #endif /* INIT_H */
