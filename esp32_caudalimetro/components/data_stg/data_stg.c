5112dfd814f6cc3950d7b0afb78c9b42d5d8ab92
AlexisDeLaCruzHernandez <alexisdelacruzhernandez@gmail.com>
2026-08-28 21:18:57 -0300

    feat: crear componente que controla la memoria flash interna

---
+++ esp32_caudalimetro/components/data_stg/data_stg.c
@@ -0,0 +1,172 @@
+#include "data_stg.h"
+#include "esp_log.h"
+
+static const char *TAG = "DATA_STG";
+
+esp_err_t data_stg_mount(void) 
+{
+    data_stg_config_t data_stg_config = {
+        .base_path = BASE_PATH,
+        .partition_label = PARTITION_LABEL,
+        .mount_config = {
+            .max_files = 1,
+            .format_if_mount_failed = false,
+            .allocation_unit_size = CONFIG_WL_SECTOR_SIZE,
+        }
+    };
+
+    esp_err_t ret = esp_vfs_fat_spiflash_mount_rw_wl(
+        data_stg_config.base_path, 
+        data_stg_config.partition_label, 
+        &data_stg_config.mount_config, 
+        &data_stg_config.wl_handle
+    );
+    if(ret == ESP_OK) ESP_LOGI(TAG, "Montaje de sistema de archivos FAT exitoso");
+    else ESP_LOGE(TAG, "Error al montar el sistema de archivos FAT");
+    
+    return ret;
+}
+
+esp_err_t data_stg_write_measurement(data_t *data) 
+{
+    char file_path[64];
+    struct tm time_info;
+    time_t time = data->time_info;
+    localtime_r(&time, &time_info);
+
+    int year = time_info.tm_year + 1900;
+    int month = time_info.tm_mon + 1;
+
+    // El nombre del archivo viene dado por el año y el mes del timestamp
+    snprintf(file_path, sizeof(file_path), BASE_PATH "/%04d-%02d.bin", year, month);
+
+    // Abre el archivo en modo append binario
+    FILE *f = fopen(file_path, "ab");
+    if(f == NULL) {
+        ESP_LOGE(TAG, "Error al abrir el archivo: %s", file_path);
+        return ESP_FAIL;
+    }
+
+    // Escribimos el registro en flash
+    if(fwrite(data, sizeof(data_t), 1, f) != 1) {
+        ESP_LOGE(TAG, "Error al escribir en el archivo: %s", file_path);
+        fclose(f);
+        return ESP_FAIL;
+    }
+
+    // Cerramos el archivo
+    fclose(f);
+
+    // ESP_LOGI(TAG, "Archivo (%s) escrito correctamente", file_path);
+    return ESP_OK;
+}
+
+esp_err_t data_stg_read_range(time_t *start_time, time_t *end_time, data_t *buffer, size_t max_size, size_t *items_read)
+{
+    // Convertimos las fechas a struct tm para una obtener años y meses
+    struct tm start_date, end_date;
+    localtime_r(start_time, &start_date);
+    localtime_r(end_time, &end_date);
+
+    // Nos fijamos el rango indicado en los parametros
+    if(*start_time == -1 || *end_time == -1 || *start_time > *end_time) {
+        ESP_LOGE(TAG, "Rango de fechas inválido");
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    // Guardamos el año y el mes final e inicial para abrir los archivos
+    int current_year = start_date.tm_year + 1900;
+    int current_month = start_date.tm_mon + 1;
+    int end_year = end_date.tm_year + 1900;
+    int end_month = end_date.tm_mon + 1;
+
+    bool done = false;
+    size_t count = 0;
+
+    while(!done) {
+        char file_path[64];
+        // El nombre del archivo viene dado por el año y el mes del timestamp
+        snprintf(file_path, sizeof(file_path), BASE_PATH "/%04d-%02d.bin", current_year, current_month);
+
+        // Abrimos el archivo en modo lectura
+        FILE *f = fopen(file_path, "rb");
+        if(f != NULL) {
+            bool file_done = false;
+
+            while(!file_done) {
+                // Nos fijamos el espacio disponible en el buffer
+                size_t remaining_space = max_size - count;
+                // Nos fijamos cuanto se leyó para detectar fin de archivo
+                size_t elements_read = fread(&buffer[count], sizeof(data_t), remaining_space, f);
+                // Variable para guardar lecturas dentro del rango
+                size_t valid_read = 0;
+                for(size_t i = 0; i < elements_read; i++) {
+                    // Analizamos cada muestra para ver si el dato entra dentro del rango
+                    data_t temp = buffer[count + i];
+                    time_t time_info = (time_t)temp.time_info;
+                    if(time_info >= *start_time && time_info <= *end_time) {
+                        // Movemos los datos a la izquierda para sacar datos fuera de rango
+                        buffer[count + valid_read] = temp;
+                        valid_read++;
+                        *start_time = time_info + 1;
+                    }
+                    else if(time_info >= *end_time) {
+                        // Cerramos el archivo
+                        fclose(f);
+                        // actualizamos tiempo para saber que se terminó la lectura
+                        *start_time = *end_time;
+                        count += valid_read;
+                        *items_read = count;
+                        ESP_LOGI(TAG, "Lectura completada, por end_time");
+                        return ESP_OK;
+                    }
+                }
+                count += valid_read;
+                if(count == max_size) {
+                    fclose(f);
+                    *items_read = count;
+                    ESP_LOGI(TAG, "Buffer lleno");
+                    return ESP_OK;
+                }
+                if(elements_read < remaining_space) {
+                    if (ferror(f)) {
+                        ESP_LOGE(TAG, "Error de I/O al leer: %s", file_path);
+                        fclose(f);
+                        *items_read = count;
+                        return ESP_FAIL;
+                    }
+                    file_done = true;
+                }
+            }
+            fclose(f);
+        }
+        else {
+            if (errno == ENOENT) {
+                ESP_LOGW(TAG, "Mes no disponible: %s", file_path);
+            } else {
+                ESP_LOGE(TAG, "Error al abrir: %s", file_path);
+                *items_read = count;
+                return ESP_FAIL;
+            }
+        }
+
+        // En caso de que la fecha actual sea la final se terminó la lectura
+        if(current_year == end_year && current_month == end_month) {
+            done = true;
+        }
+        // Caso contrario pasamos al siguiente mes/año
+        else {
+            current_month++;
+            if(current_month > 12) {
+                current_month = 1;
+                current_year++;
+            }
+        }
+    }
+
+    // actualizamos tiempo para saber que se terminó la lectura
+    *start_time = *end_time;
+    *items_read = count;
+    ESP_LOGI(TAG, "Lectura completada, fin de archivos");
+    return ESP_OK;
+}
---
+++ esp32_caudalimetro/components/data_stg/include/data_stg.h
@@ -0,0 +1,55 @@
+#ifndef DATA_STG_H
+#define DATA_STG_H
+
+#include <time.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include "esp_vfs_fat.h"
+
+#define BASE_PATH           "/flash"
+#define PARTITION_LABEL     "storage"        
+
+typedef struct {
+    uint32_t time_info;
+    uint16_t volume;
+} __attribute__((packed)) data_t;
+
+typedef struct {
+    const char *base_path;
+    const char *partition_label;
+    esp_vfs_fat_mount_config_t mount_config;
+    wl_handle_t wl_handle;
+} data_stg_config_t;
+
+/**
+ * @brief                           Monta el sistema de archivos FAT de la tarjeta partición.
+ * @retval ESP_OK                   Montaje de sistema de archivos FAT exitoso.
+ * @retval Other                    Ocurrió un error al montar el sistema de archivos FAT.
+ */
+esp_err_t data_stg_mount(void);
+
+/**
+ * @brief                           Escribe una medición junto al timestamp en la memoria flash.
+ * @param[in] data                  Puntero a la estructura que contiene la información a registrar.
+ * @retval ESP_OK                   Escritura completada y confirmada en memoria correctamente.
+ * @retval ESP_FAIL                 Error en apertura o escritura del archivo.
+ */
+esp_err_t data_stg_write_measurement(data_t *data);
+
+/**
+ * @brief                           Lee mediciones en determinado rango de fechas.
+ * @param[in] start_time            Fecha inicial del rango a buscar.
+ * @param[in] end_time              Fecha final del rango a buscar.
+ * @param[out] buffer               Puntero al buffer de salida donde se almacenarán las mediciones.
+ * @param[in] max_size              Cantidad máxima de elementos que puede almacenar el buffer.
+ * @param[in] items_read            Cantidad de elementos almacenados en el buffer.
+ * @retval ESP_OK                   Lectura finalizada con éxito.
+ * @retval ESP_FAIL                 Error en apertura o lectura del archivo.
+ * @retval ESP_ERR_INVALID_ARG      Fechas nulas o rango incoherente (start_date > end_date).
+ */
+esp_err_t data_stg_read_range(time_t *start_time, time_t *end_time, data_t *buffer, size_t max_size, size_t *items_read);
+
+#endif /* DATA_STG_H */
