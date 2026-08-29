988500c5805527ba6557c0e94dee2b2001811e24
AlexisDeLaCruzHernandez <alexisdelacruzhernandez@gmail.com>
2026-08-28 21:19:23 -0300

    feat: modificar código para utilizar la memoria flash

--- esp32_caudalimetro/main/init.c
+++ esp32_caudalimetro/main/init.c
@@ -2,31 +2,3 @@
 #include "esp_log.h"
 
 static const char *TAG = "INIT";
-
-esp_err_t init_sd_card(sd_card_config_t *sd_card_config) 
-{
-    sd_card_config->base_path = MOUNT_POINT;
-
-    sd_card_config->host_config_input = (sdmmc_host_t)SDSPI_HOST_DEFAULT();
-
-    sd_card_config->slot_config = (sdspi_device_config_t)SDSPI_DEVICE_CONFIG_DEFAULT();
-    sd_card_config->slot_config.gpio_cs = PIN_NUM_CS;
-    sd_card_config->slot_config.host_id = sd_card_config->host_config_input.slot;
-
-    sd_card_config->mount_config.format_if_mount_failed = false;
-    sd_card_config->mount_config.max_files = 1;
-    sd_card_config->mount_config.allocation_unit_size = 16 * 1024;
-
-    spi_bus_config_t bus_config = {
-        .mosi_io_num = PIN_NUM_MOSI,
-        .miso_io_num = PIN_NUM_MISO,
-        .sclk_io_num = PIN_NUM_SCK,
-        .quadwp_io_num = -1,
-        .quadhd_io_num = -1,
-        .max_transfer_sz = 4 * 1024,
-    };
-
-    esp_err_t ret = spi_bus_initialize(sd_card_config->host_config_input.slot, &bus_config, SDSPI_DEFAULT_DMA);
-    if(ret == ESP_OK) ESP_LOGI(TAG, "sd_card_config y bus spi inicializados");
-    return ret;
-}
--- esp32_caudalimetro/main/main.c
+++ esp32_caudalimetro/main/main.c
@@ -1,72 +1,100 @@
 #include <stdio.h>
 
-#include "sd_card.h"
+#include "esp_log.h"
+
+#include "data_stg.h"
 #include "init.h"
 
+#include <dirent.h>
+
 static const char *TAG = "MAIN";
+
+void borrar_todos_los_archivos(const char *path) {
+    DIR *dir = opendir(path);
+    if (dir == NULL) return;
+
+    struct dirent *entry;
+    char filepath[300];
+
+    ESP_LOGI(TAG, "--- Eliminando todos los archivos ---");
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) continue;
+        snprintf(filepath, sizeof(filepath), "%s/%s", path, entry->d_name);
+        remove(filepath);
+    }
+    closedir(dir);
+}
 
 void app_main(void)
 {
     data_t data;
-    sd_card_config_t sd_card_config;
-    ESP_ERROR_CHECK(init_sd_card(&sd_card_config));
+    data_t buffer[BUFFER_SIZE];
 
-    while(sd_card_mount(&sd_card_config) != ESP_OK) {
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-    
-    struct tm time_info = {
-        .tm_year = 126,
-        .tm_mon = 7,
-        .tm_mday = 20,
+    ESP_LOGI(TAG, "Montando sistema de archivos FAT");
+    ESP_ERROR_CHECK(data_stg_mount());
+
+    // borrar_todos_los_archivos(BASE_PATH);
+
+    data.volume = 10;
+    struct tm initial_time = {
+        .tm_year = 2026 - 1900,
+        .tm_mon = 10 - 1,
+        .tm_mday = 1,
         .tm_hour = 0,
         .tm_min = 0,
         .tm_sec = 0,
     };
-    data.volume = 0;
-    data.time_info = mktime(&time_info);
-    /*
-    for(uint32_t i = 0; i < 1000; i++) {
-        sd_card_write_measurement(&data);
-        data.volume += 10;
-        data.time_info.tm_min += 15;
-        if(data.time_info.tm_min == 60) {
-            data.time_info.tm_min = 0;
-            data.time_info.tm_hour++;
-            if(data.time_info.tm_hour == 24) {
-                data.time_info.tm_hour = 0;
-                data.time_info.tm_mday++;
-            }
+    data.time_info = (uint32_t)mktime(&initial_time);
+
+    uint32_t time_interval = 30 * 24 * 60 * 60; // 30 días del mes
+
+    ESP_LOGI(TAG, "Escribiendo datos");
+
+    time_t time_sec = (time_t)data.time_info;
+    struct tm time;
+    localtime_r(&time_sec, &time);
+    ESP_LOGI(TAG, "Fecha inicial: [%04d/%02d/%02d %02d:%02d:%02d]", 
+        time.tm_year + 1900, time.tm_mon + 1, time.tm_mday, 
+        time.tm_hour, time.tm_min, time.tm_sec
+    );
+
+    for(uint32_t i = 0; i < time_interval / INTERVAL_SEC; i++) {
+        // data_stg_write_measurement(&data);
+        data.time_info += INTERVAL_SEC;
+    }
+
+    time_sec = (time_t)data.time_info;
+    localtime_r(&time_sec, &time);
+    ESP_LOGI(TAG, "Fecha final: [%04d/%02d/%02d %02d:%02d:%02d]", 
+        time.tm_year + 1900, time.tm_mon + 1, time.tm_mday, 
+        time.tm_hour, time.tm_min, time.tm_sec
+    );
+
+    time_t start_time = mktime(&initial_time);
+    time_t end_time = time_sec -= INTERVAL_SEC;
+
+    ESP_LOGI(TAG, "Leyendo datos");
+    size_t items_read = 0, total_read = 0;
+    while(start_time != end_time) {
+        data_stg_read_range(&start_time, &end_time, buffer, BUFFER_SIZE, &items_read);
+        ESP_LOGI(TAG, "Leidos: %d", items_read);
+        if(total_read == 0) {
+            time_sec = (time_t)buffer[0].time_info;
+            localtime_r(&time_sec, &time);
+            ESP_LOGI(TAG, "Primer leido: [%04d/%02d/%02d %02d:%02d:%02d] %d", 
+                time.tm_year + 1900, time.tm_mon + 1, time.tm_mday, 
+                time.tm_hour, time.tm_min, time.tm_sec, buffer[0].volume
+            );
         }
+        total_read += items_read;
     }
-    */
-
-    struct tm start_date = {
-        .tm_hour = 0,
-        .tm_min = 0,
-        .tm_sec = 0,
-        .tm_mday = 20,
-        .tm_mon = 7,
-        .tm_year = 126,
-    };
-    struct tm end_date = {
-        .tm_hour = 9,
-        .tm_min = 45,
-        .tm_sec = 0,
-        .tm_mday = 30,
-        .tm_mon = 7,
-        .tm_year = 126,
-    };
-
-    sdmmc_card_print_info(stdout, sd_card_config.card);
-
-    ESP_LOGI(TAG, "Comienzo lectura");
-    // sd_card_read_range(&start_date, &end_date);
-
-    esp_vfs_fat_sdcard_unmount(sd_card_config.base_path, sd_card_config.card);
-    ESP_LOGI(TAG, "SD desmontada");
-
-    spi_bus_free(sd_card_config.host_config_input.slot);
+    ESP_LOGI(TAG, "Leidos totales: %d", total_read);
+    time_sec = (time_t)buffer[items_read - 1].time_info;
+    localtime_r(&time_sec, &time);
+    ESP_LOGI(TAG, "Ultimo leido: [%04d/%02d/%02d %02d:%02d:%02d] %d", 
+        time.tm_year + 1900, time.tm_mon + 1, time.tm_mday, 
+        time.tm_hour, time.tm_min, time.tm_sec, buffer[items_read - 1].volume
+    );
 
     while(1) {
         vTaskDelay(pdMS_TO_TICKS(1000));
