8e389213b9e22e8686c358c3f44881b8afcd1745
AlexisDeLaCruzHernandez <alexisdelacruzhernandez@gmail.com>
2026-08-30 15:32:27 -0300

    feat: añadir funciones de inicializacion de wifi y gpio

--- esp32_caudalimetro/main/init.c
+++ esp32_caudalimetro/main/init.c
@@ -3,7 +3,8 @@
 
 static const char *TAG = "INIT";
 
-void borrar_todos_los_archivos(const char *path) {
+void borrar_todos_los_archivos(const char *path) 
+{
     DIR *dir = opendir(path);
     if (dir == NULL) return;
 
@@ -17,4 +18,50 @@ void borrar_todos_los_archivos(const char *path) {
         remove(filepath);
     }
     closedir(dir);
+}
+
+void wifi_init_sta(EventGroupHandle_t *wifi_event, esp_event_handler_t handler)
+{
+    *wifi_event = xEventGroupCreate(); // Crea el event group 
+    ESP_ERROR_CHECK(esp_netif_init()); // Inicializa la pila de red TCP/IP
+    ESP_ERROR_CHECK(esp_event_loop_create_default()); // Crea loop de eventos para distribuirlos 
+    esp_netif_create_default_wifi_sta(); // Crea la interfaz de red para el modo STA
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT(); // Obtiene la configuración por defecto para inicializar el Wi-Fi
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg)); // Inicializa el Wi-Fi
+
+    // Registra el Handler para eventos relacionados a la conexión Wi-Fi
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, handler, NULL));
+    // Registra el Handler para eventos relacionados con la dirección IP
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, handler, NULL));
+
+    wifi_config_t wifi_config = { // Configuración de la red Wi-Fi
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS,
+        },
+    };
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA)); // Indica el modo STA
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config)); // Carga la configuración de la red
+    ESP_ERROR_CHECK(esp_wifi_start()); // Inicia el controlador de Wi-Fi, provoca WIFI_EVENT_STA_START
+}
+
+void gpio_caudal_init(gpio_isr_t isr_handler)
+{
+    gpio_config_t io_conf = {
+        .pin_bit_mask = (1ULL << CAUDAL_PIN),
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = 1,               // Activamos pull-up interno
+        .pull_down_en = 0,
+        .intr_type = GPIO_INTR_NEGEDGE // ¡CLAVE! Solo interrumpe al bajar el flanco
+    };
+    gpio_config(&io_conf);
+
+    // 4. Instalamos el servicio global de interrupciones
+    // El parámetro (0) indica que no estamos pasando flags especiales de asignación
+    gpio_install_isr_service(0);
+    
+    // 5. Enganchamos nuestra ISR específica al pin del botón
+    gpio_isr_handler_add(CAUDAL_PIN, isr_handler, NULL);
 }
--- esp32_caudalimetro/main/init.h
+++ esp32_caudalimetro/main/init.h
@@ -4,11 +4,24 @@
 #include <stdio.h>
 #include <dirent.h>
 
+#include "esp_wifi.h"
+#include "driver/gpio.h"
+
 #define BUFFER_SIZE         256
 #define INTERVAL_MIN        15
 #define INTERVAL_SEC        (INTERVAL_MIN * 60) 
+
 #define INTERVAL_MAX_LITER  ((5000 / 60) * INTERVAL_MIN)
+#define CAUDAL_PIN          GPIO_NUM_0 
+#define DEBOUNCE_TIME       20      // 20 ms 
+
+#define WIFI_SSID           "Speedy-Fibra"
+#define WIFI_PASS           "casa1234"
 
 void borrar_todos_los_archivos(const char *path);
+
+void wifi_init_sta(EventGroupHandle_t *wifi_event, esp_event_handler_t handler);
+
+void gpio_caudal_init(gpio_isr_t isr_handler);
 
 #endif /* INIT_H */
