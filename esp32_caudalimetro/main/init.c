43cbcf2dd38a8fd8fb791f795c65a1d6207ca92a
AlexisDeLaCruzHernandez <alexisdelacruzhernandez@gmail.com>
2026-08-30 15:54:15 -0300

    feat: añadir inicialización del gpio para caudalimetro

--- esp32_caudalimetro/main/init.c
+++ esp32_caudalimetro/main/init.c
@@ -3,7 +3,8 @@
 
 static const char *TAG = "INIT";
 
-void borrar_todos_los_archivos(const char *path) {
+void borrar_todos_los_archivos(const char *path) 
+{
     DIR *dir = opendir(path);
     if (dir == NULL) return;
 
@@ -17,4 +18,39 @@ void borrar_todos_los_archivos(const char *path) {
         remove(filepath);
     }
     closedir(dir);
+}
+
+esp_err_t gpio_caudal_init(gpio_isr_t isr_handler)
+{
+    esp_err_t ret;
+    gpio_config_t io_conf = {
+        .pin_bit_mask = (1ULL << CAUDAL_PIN),
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = 1,               // Pull up
+        .pull_down_en = 0,
+        .intr_type = GPIO_INTR_NEGEDGE // Interrupción flanco descendente
+    };
+
+    // Configuramos el GPIO
+    ret = gpio_config(&io_conf);
+    if(ret != ESP_OK) {
+        ESP_LOGE(TAG, "Error al configurar el GPIO");
+        return ret;
+    }
+
+    // Instalamos las interrupciones
+    ret = gpio_install_isr_service(0);
+    if(ret != ESP_OK) {
+        ESP_LOGE(TAG, "Error al instalar las interrupciones");
+        return ret;
+    }
+    
+    // Añadimos el handler de interrupción
+    ret = gpio_isr_handler_add(CAUDAL_PIN, isr_handler, NULL);
+    if(ret != ESP_OK) {
+        ESP_LOGE(TAG, "Error al añadir el handler de interrupción");
+        return ret;
+    }
+
+    return ESP_OK;
 }
--- esp32_caudalimetro/main/init.h
+++ esp32_caudalimetro/main/init.h
@@ -4,11 +4,28 @@
 #include <stdio.h>
 #include <dirent.h>
 
+#include "driver/gpio.h"
+
 #define BUFFER_SIZE         256
 #define INTERVAL_MIN        15
 #define INTERVAL_SEC        (INTERVAL_MIN * 60) 
-#define INTERVAL_MAX_LITER  ((5000 / 60) * INTERVAL_MIN)
 
+#define INTERVAL_MAX_LITER  ((5000 / 60) * INTERVAL_MIN)
+#define CAUDAL_PIN          GPIO_NUM_0
+#define DEBOUNCE_TIME_MS    20       
+
+/**
+ * @brief                   Borra todos los archivos del directorio
+ * @param[in] path          String con directorio base a borrar
+ */
 void borrar_todos_los_archivos(const char *path);
+
+/**
+ * @brief                   Inicializa el gpio con pull up e interrupción
+ * @param[in] isr_handler   Handler de interrupcion
+ * @retval ESP_OK           Inicialización correcta del GPIO
+ * @retval Other            Ocurrió un error en la inicialización
+ */
+esp_err_t gpio_caudal_init(gpio_isr_t isr_handler);
 
 #endif /* INIT_H */
